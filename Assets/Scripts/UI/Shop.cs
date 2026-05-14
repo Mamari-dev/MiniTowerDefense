20389a659eff45aec46f810d4aac304df6e02378
Mamari-dev <Mariomay92@googlemail.com>
2026-05-14 18:59:04 +0200

    14.05.2026
    Create UI Canvas
    -> Start Button (without code)
    -> Currency overlay (without code)
    -> Shop (working on it - little code for movment window)

---
+++ Assets/Scripts/UI/Shop.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class Shop : MonoBehaviour
+{
+    private int openPosition = 0;
+    private float closePosition;
+    private bool isShopOpen = false;
+
+    [SerializeField] private float speed;
+
+    private RectTransform rectTransform;
+    private Coroutine moveCoroutine;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        closePosition = rectTransform.rect.width;
+    }
+
+    public void OnShopButtonClick()
+    {
+        if (moveCoroutine != null)
+            StopCoroutine(moveCoroutine);
+
+        if (isShopOpen)
+        {
+            moveCoroutine = StartCoroutine(MoveShopWindow(closePosition));
+            isShopOpen = false;
+        }
+        else
+        {
+            moveCoroutine = StartCoroutine(MoveShopWindow(openPosition));
+            isShopOpen = true;
+        }
+    }
+
+    private IEnumerator MoveShopWindow(float goalPosition)
+    {
+        yield return new WaitUntil(() =>
+            {
+                Vector2 newPos = new(Mathf.MoveTowards(rectTransform.anchoredPosition.x, goalPosition, speed * Time.deltaTime), rectTransform.anchoredPosition.y);
+                rectTransform.anchoredPosition = newPos;
+
+                return Mathf.Approximately(rectTransform.anchoredPosition.x, goalPosition);
+            });
+
+        moveCoroutine = null;
+    }
+}
